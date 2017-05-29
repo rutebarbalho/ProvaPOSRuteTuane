56edfed4eb901e7d8b1f552ca182b451a9ec7425
leonardo <leonardo.minora@gmail.com>
2017-05-29 09:50:55 -0300

    ajuda do professor mais querido do mundo :D

--- solucao.js
+++ solucao.js
@@ -1,54 +1,64 @@
-var nomeArquivo = process.argv[2];
-var n;
+var fs = require('fs')
+
+main();
+
+function main() {
+	var nomeArquivo = process.argv[2];
+	var n;
+
+	var e = lerEntrada(nomeArquivo);
+	if (e == 'json'){
+		lerJson(nomeArquivo)
+	}
+	if (e == 'csv'){
+		lerCsv(nomeArquivo)
+	}
+	if (e == 'xml'){
+		lerXML(nomeArquivo)
+	}
+
+}
+
 function lerEntrada(nomeArquivo){
 	var index = nomeArquivo.lastIndexOf(".")
 	var extensao = nomeArquivo.substring(index + 1, nomeArquivo.length);
 	return extensao;
 }
-var e = lerEntrada(nomeArquivo);
-if (e == 'json'){
-	lerJson()
-}
-if (e == 'csv'){
-	lerCsv()
-}
-if (e == 'xml'){
-	lerXML()
-}
+
 function lerCsv(nomeArquivo){
-	var fs = require('fs')
 	var csv = require('csv-string')
 	var arquivo = __dirname + '/' + nomeArquivo;
 	var resultado;
 	fs.readFile(arquivo, 'utf8', (err, data) => {
-	    resultado = csv.parse(data);
-		Intervalos()
-})
-	function lerXML(nomeArquivo, funcao) {
-		var arquivo = __dirname + '/' + nomeArquivo;
-		var n = [];
-	    var fs = require('fs');
-	    var xmlParser = require('xml2js').parseString;
+		resultado = csv.parse(data);
+		Intervalos(resultado)
+	})
+}
+function lerXML(nomeArquivo) {
+	var arquivo = __dirname + '/' + nomeArquivo;
+	var n = [];
+	var xmlParser = require('xml2js').parseString;
 
-	    fs.readFile(nomeArquivo, function(err, data) {
-	        xmlParser(data, funcao);
-				})
-				Intervalos()
+	fs.readFile(nomeArquivo, function(err, data) {
+		xmlParser(data, function(err, result) {
+			Intervalos(result);
+		});
+	})
 }
 function lerJson(nomeArquivo) {
 	var json = require(__dirname + '/' + nomeArquivo);
 	console.log(json);
 	var fs = require("fs");
 	console.log(json[0].numeros);
-	n = json[0].numeros.split(",");
-	Intervalos()
+	var n = json[0].numeros.split(",");
+	Intervalos(n)
 }
-function Intervalos() {
+function Intervalos(n) {
 	var grupo = [];
 	var grupos = [];
 	for (var x=0; x<n.length; x++){
 		if(grupo.length == 0) grupo.push(n[x])
-		else if(n[x] - 1 == n[x-1]) conjunto.push(n[x])
+		else if(n[x] - 1 == n[x-1]) grupo.push(n[x])
 		else{
 			grupos.push(grupo)
 			grupo = []
@@ -57,15 +67,18 @@ function Intervalos() {
 	}
 	var intervalo = [];
 	var numInicial = n[0];
-		for(i=0; i < n.length; i++)	{
-		var grupo = grupos[i];
+	console.log(grupo);
+	for(i=0; i < n.length; i++)	{
+		// var grupo = grupos[i];
 		if(grupo.length == 1) intervalo.push("[" + numInicial + "]")
 		else intervalo.push("[" + grupo[0] + "-" + grupo[grupo.length-1] + "]")
-			}
-			salvar_arquivo("/intervalos.json", intervalo)
-		}
-function salvar_arquivo() {
-	fs.writeFile('intervalos.json', JSON.stringify(intervalo), function (err) {
-	if (err) return console.log(err);
-	console.log('Arquivo salvo');
-})
+	}
+	salvar_arquivo("./intervalos.json", intervalo)
+}
+
+function salvar_arquivo(arq, intervalo) {
+	fs.writeFile(arq, intervalo, function (err) {
+		if (err) return console.log(err);
+		console.log('Arquivo salvo');
+	})
+}
